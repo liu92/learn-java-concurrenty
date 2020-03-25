c78b3713c5f844b446fda83c28309953e89b38c8
liu9209 <470437289@qq.com>
2020-03-25 23:10:55 +0800

    update：wait set的简单说明

---
+++ src/main/java/com/learn/concurrenty/design/chapter2/WaitSet.java
@@ -0,0 +1,58 @@
+package com.learn.concurrenty.design.chapter2;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+/**
+ * 多线程的休息室WaitSet
+ * @ClassName: WaitSet
+ * @Description:
+ * @Author: lin
+ * @Date: 2020/3/25 22:40
+ * History:
+ * @<version> 1.0
+ */
+public class WaitSet {
+
+    private static  final Object LOCK = new Object();
+
+
+    /**
+     *  wait set 在jvm中明确的规定
+     * 1.所有的对象都会有一个wait set,用来存放调用了该对象wait方法之后进入block状态线程
+     * 2.线程被notify之后，不一定立即得到执行
+     * 3.线程从 wait set中被唤醒顺序不一定是 FIFO.
+     * @param args
+     */
+    public static void main(String[] args) {
+        IntStream.rangeClosed(1, 10).forEach(i ->
+                new Thread(String.valueOf(i)){
+                    @Override
+                    public void run() {
+                        synchronized (LOCK){
+
+                            try {
+                                Optional.of(Thread.currentThread().getName() + " will come to wait set.").ifPresent(System.out::println);
+                                LOCK.wait();
+                                Optional.of(Thread.currentThread().getName() + " will leave to wait set.").ifPresent(System.out::println);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }.start()
+        );
+
+
+        IntStream.rangeClosed(1, 10).forEach(i ->{
+            synchronized (LOCK){
+                LOCK.notify();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
