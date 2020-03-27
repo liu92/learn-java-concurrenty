898baa9152a64ecf7f675f287474d68e837fc05b
liu9209 <470437289@qq.com>
2020-03-27 23:21:06 +0800

    update:ThreadLocal实现上下文

---
+++ src/main/java/com/learn/concurrenty/design/chapter11/ExecutionTask.java
@@ -0,0 +1,32 @@
+package com.learn.concurrenty.design.chapter11;
+
+/**
+ * 多线程运行上下文切换
+ * @ClassName: ExecutionTask
+ * @Description:
+ * @Author: lin
+ * @Date: 2020/3/27 22:20
+ * History:
+ * @<version> 1.0
+ */
+public class ExecutionTask implements Runnable {
+    private QueryFromDbAction action = new QueryFromDbAction();
+    private QueryFromHttpAction httpAction = new QueryFromHttpAction();
+    /**
+     */
+    @Override
+    public void run() {
+         Context context = ActionContext.getActionContext().getContext();
+       action.execute();
+       System.out.println("The name query successful");
+       httpAction.execute();
+       System.out.println("The card id query successful");
+       System.out.println("The Name is " + context.getName() +" and CardId " + context.getCardId());
+    }
+
+
+
+
+
+
+}
---
+++ src/main/java/com/learn/concurrenty/design/chapter12/BalkingData.java
@@ -0,0 +1,61 @@
+package com.learn.concurrenty.design.chapter12;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * 这个就是 当一个人看到 文件已经被写入进去了，那么就不在 执行写的操作
+ * @ClassName: BalkingData
+ * @Description:
+ * @Author: lin
+ * @Date: 2020/3/27 23:05
+ * History:
+ * @<version> 1.0
+ */
+public class BalkingData {
+    private final  String fileName;
+
+    private String content;
+    private boolean change;
+
+    BalkingData(String fileName, String content){
+        this.fileName = fileName;
+        this.content = content;
+        this.change = true;
+    }
+
+    /**
+     * 一个线程会取改变它
+     * @param newContent
+     */
+    public  synchronized  void change(String newContent){
+        this.content = newContent;
+        this.change = true;
+    }
+
+    /**
+     * 巡视的方法，看看需不需要保存
+     * @throws IOException
+     */
+    public synchronized  void  save() throws IOException {
+        if(!change){
+            return;
+        }
+        doSave();
+        //述求已经被受理了
+        this.change = false;
+    }
+
+    private void doSave() throws IOException {
+        System.out.println(Thread.currentThread().getName() + "calls so save, content=" + content);
+        try(Writer writer = new FileWriter(fileName,true)) {
+            writer.write(content);
+            writer.write("\n");
+            writer.flush();
+            // 最后还要去关掉它，所以就用try/catch的方式去做
+        }
+    }
+
+
+}
